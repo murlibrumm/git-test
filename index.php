f6d43dd461b4fbfb3babc38d2c28b85ecf65882e
Wolfgang Weintritt <wolfgang.weintritt@gmail.com>
2017-11-15 22:58:13 +0100

    Add index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$name = "WW";
+var_dump($name);
+$name++;
+
+$name = null;
+//$name->toString(); // to produce errors in php_error.log
+
+var_dump($name);
+
+print_r (phpinfo());
+
+// TODO
+// git rebase
+// git pull request
+
+//Save string to log, use FILE_APPEND to append.
+file_put_contents('./log_'.date("j.n.Y").'.txt', 'some log message, yo', FILE_APPEND);
+
+?>
